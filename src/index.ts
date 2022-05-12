ec350a52eb9808616c43080739326c3bcaff477d
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-05-12 13:10:46 -0400

    chore: add index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import CommandListener from './commands/CommandListener';
+import SlashCommand from './commands/slash/SlashCommand';
+
+exports.CommandListener = CommandListener;
+exports.SlashCommand = SlashCommand;
